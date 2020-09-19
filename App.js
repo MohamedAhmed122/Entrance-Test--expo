e5e13b54f9aab1f375369fb3ae8b05bb328e8f62
MohamedAhmed122 <mohamed.ahm9833@gmail.com>
2020-09-19 10:36:47 +0700

    Structure the Project

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Item from './app/component/Store/Item';
+import WrapperScreen from './app/screens/WrapperScreen'
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Item />
     </View>
   );
 }
